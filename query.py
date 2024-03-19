c6f7b80a7f83b61f281968ced4f78ede881c3ed2
Huai <chrbezz0487@gmail.com>
2024-03-19 10:16:39 +0800

    readme update

--- query.py
+++ query.py
@@ -5,7 +5,7 @@ from pprint import pprint
 
 from model.sentence_embedding import get_sentence_embedding
 
-def vector_search(item, query_vector, index_name, es):
+def vector_search(item, query_vector, index_name, es, query_size=2):
     query_vector = q_embedding.tolist()  # 將張量轉換為列表
     search_params = {
         "query": {
@@ -19,10 +19,10 @@ def vector_search(item, query_vector, index_name, es):
         }
     }
     
-    result = es.search(index=index_name, body=search_params, from_=0, size=2)
+    result = es.search(index=index_name, body=search_params, from_=0, size=query_size)
     return result['hits']['hits']
 
-def query(item, query, index_name, es):
+def query(item, query, index_name, es, query_size=2):
     
     # Query DSL
     search_params = {
@@ -32,31 +32,33 @@ def query(item, query, index_name, es):
     }
     
     #Search document
-    result = es.search(index=index_name, body=search_params, from_=0, size=2)
+    result = es.search(index=index_name, body=search_params, from_=0, size=query_size)
     
     return result['hits']['hits']
 
 if __name__ == "__main__":   
-    # item, q = sys.argv[1], sys.argv[2] 
     
     # : Connect Setting
     index_name = 'ncu_lrn' # = create name.
     es = Elasticsearch(hosts=["http://localhost:9200"])
     
+    # : Query Setting
+    query_size = 4
     item = 'content'
     # q = input("your query:") 
-    q = "張立杰教授"
+    q = "張立杰教授的個人簡例和研究計畫"
     q_embedding = get_sentence_embedding(q)
-    results = query(item, q, index_name, es) 
+    results = query(item, q, index_name, es, query_size) 
     
     print("搜尋語句:", q, "搜尋欄位:", item)
+    print("------------------------------------------------")
     
     print("未使用 Embedding 搜尋結果:")
     for res in results:
         print("URL:", res["_source"]['url'])
         
     print("@ 使用 Embedding 搜尋結果:")
-    results = vector_search(item, q_embedding, index_name, es)
+    results = vector_search(item, q_embedding, index_name, es, query_size)
     for res in results:
         print("URL:", res["_source"]['url'])
     
